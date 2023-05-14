23c743f037e2d107fe3daf1bd591f1d8adbcffea
Jordi <jordi.lanau@and.digital>
2023-05-14 18:40:59 +0100

    Remove unnecessary test from Button component

--- src/components/Button/Button.spec.tsx
+++ src/components/Button/Button.spec.tsx
@@ -1,30 +1,11 @@
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import { Button } from './Button';
 
-const logSpy = jest.spyOn(global.console, 'log');
-
 describe('Button', () => {
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
-
   it('renders the Button component', () => {
-    render(<Button span='1' value='1' />);
+    render(<Button span='1' value='1' type='digit' />);
     const button = screen.getByRole('button', { name: '1' });
 
     expect(button).toBeInTheDocument();
-  });
-
-  it('passes the right value', async () => {
-    render(<Button span='1' value='1' />);
-    const button = screen.getByRole('button', { name: '1' });
-
-    const user = userEvent.setup();
-    await user.click(button);
-
-    expect(logSpy).toHaveBeenCalled();
-    expect(logSpy).toHaveBeenCalledTimes(1);
-    expect(logSpy).toHaveBeenCalledWith('1');
   });
 });
